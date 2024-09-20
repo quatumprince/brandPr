67c3201a4d3eb9f027b8fa4b040d65fdc28a1076
priyanshukr449 <priyanshukr449@gmail.com>
2024-09-20 18:32:08 +0530

    build check done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { BackgroundBeams } from "@/components/ui/background-beams";
 import { Navbar } from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
