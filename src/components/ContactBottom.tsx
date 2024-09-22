9960bf750e28e3997ddb371cb4a68ad133dfd29f
Prince <14prince.ace@gmail.com>
2024-09-22 18:08:17 +0530

    changesa

--- src/components/ContactBottom.tsx
+++ src/components/ContactBottom.tsx
@@ -4,8 +4,8 @@ const ContactBottom = () => {
 	return (
 		<div className="w-full  pt-8 mt-4 bg-[url('/images/bg.jpg')] bg-cover bg-center bg-opacity-30 h-[45vh]">
 			<div className=" mx-auto max-w-screen-xl mt-8 px-4 pb-8 sm:px-6 lg:px-8 ">
-				<h2 className="text-3xl font-bold sm:text-4xl pb-8">
-					Contact Us
+				<h2 className="text-3xl font-bold  sm:text-4xl pb-8">
+					Contact Us N
 				</h2>
 				<h4 className="w-full sm:w-1/2 mb-3 font-semibold">
 					Ready to Elevate Your Crypto Brand?
