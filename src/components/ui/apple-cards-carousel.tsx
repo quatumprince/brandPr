5f9fc2ef77f26be31a9d894965bb7f8dfb396252
priyanshukr449 <priyanshukr449@gmail.com>
2024-09-24 20:02:54 +0530

    left and right button added

--- src/components/ui/apple-cards-carousel.tsx
+++ src/components/ui/apple-cards-carousel.tsx
@@ -131,7 +131,7 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
 						))}
 					</div>
 				</div>
-				{/* <div className="flex justify-end gap-2 mr-10">
+				<div className="flex justify-end gap-2 mr-10">
 					<button
 						className="relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50"
 						onClick={scrollLeft}
@@ -146,7 +146,7 @@ export const Carousel = ({ items, initialScroll = 0 }: CarouselProps) => {
 					>
 						<MoveRight className="h-6 w-6 text-gray-500" />
 					</button>
-				</div> */}
+				</div>
 			</div>
 		</CarouselContext.Provider>
 	);
